652c5072a18222f2eefd53cf8b2ee9bc16793e95
Pranav Bhuvad <bhuvadpranav@gmail.com>
2024-12-15 10:06:01 +0530

    Task cc24  update

---
+++ flutter_user2/android/app/src/main/kotlin/com/flutter_user2/cc24/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.flutter_user2.cc24
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
